4578cab953fe2b87e6aae4a0f7f651990c9754bb
omer-algreeb <omer@jisr.net>
2020-09-23 17:19:52 +0300

    add hello controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'hellos#index'
+  resources :hellos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
